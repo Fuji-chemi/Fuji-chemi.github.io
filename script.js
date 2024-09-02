f3f9cc0c567fb34ddbe80346aef8a35ab7ffb958
Fuji-chemi <32949809+Fuji-chemi@users.noreply.github.com>
2024-09-02 16:21:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#gyozaButton').click(function() {
     $.ajax({
-      url: 'https://hooks.slack.com/services/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', // Replace with your Slack Webhook URL
+      url: 'https://hooks.slack.com/services/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', // Replace with your Slack Webhook URL
       type: 'POST',
       data: JSON.stringify({
         text: '餃子が食べたい！'
