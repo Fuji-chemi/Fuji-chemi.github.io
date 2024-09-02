8bea1e520d00f4b77a81601f0f833d8ea54831dd
Fuji-chemi <32949809+Fuji-chemi@users.noreply.github.com>
2024-09-02 16:12:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+$(document).ready(function() {
+  $('#gyozaButton').click(function() {
+    $.ajax({
+      url: 'https://hooks.slack.com/services/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', // Replace with your Slack Webhook URL
+      type: 'POST',
+      data: JSON.stringify({
+        text: '餃子が食べたい！'
+      }),
+      success: function() {
+        alert('Slackに通知が送信されました！');
+      },
+      error: function() {
+        alert('通知の送信に失敗しました。');
+      }
+    });
+  });
+});
