14621de30b6b1cbe46d0ba79b42a36d4e494a000
Fuji-chemi <32949809+Fuji-chemi@users.noreply.github.com>
2024-09-08 10:18:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $('#gyozaButton').click(function() {
     $.ajax({
-      url: 'https://hooks.slack.com/services/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', // 発行されたWebhook URLをここに貼り付けます
+      url: 'https://hooks.slack.com/services/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', // Webhook URL (ここは本来サーバーサイドで扱うべき)
       type: 'POST',
+      contentType: 'application/json',  // コンテンツタイプを指定
       data: JSON.stringify({
-        text: '餃子が食べたい！' // Slackに送信されるメッセージ
+        text: '餃子が食べたい！'
       }),
       success: function() {
         alert('Slackに通知が送信されました！');
       },
-      error: function() {
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log('エラー: ' + textStatus + ', ' + errorThrown);
         alert('通知の送信に失敗しました。');
       }
     });
